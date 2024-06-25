f79bdc98192ff01364a73bc0ba074d3ba9c70430
kashish567 <kashishbhanushali7@gmail.com>
2024-06-25 20:53:08 +0530

    feat: api to get blog by id

--- src/app/api/blogs/getallblogs/route.ts
+++ src/app/api/blogs/getallblogs/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 dbConnection();
 export const GET=async(req : NextRequest)=>{
     try{
-        const blogs = await Blog.find();
+        const blogs = await Blog.find().populate("user");
         return NextResponse.json({message:"All Blogs",data:blogs},{status:200});
     }catch(error:any){
         return NextResponse.json({message:error.message},{status:500});
---
+++ src/app/api/blogs/getblog/[id]/route.ts
@@ -0,0 +1,21 @@
+import Blog from "@/models/blogModel";
+import dbConnection from "@/config/dbConnect";
+import { NextRequest, NextResponse } from "next/server";
+import mongoose from "mongoose";
+
+dbConnection();
+
+type Params = {
+    id: string;
+  };
+
+export const GET = async (req: NextRequest, context: { params: Params }) => {
+    try {
+        const id = new mongoose.Types.ObjectId(context.params.id);
+        const blogs = await Blog.find({user:id}).populate("user");
+        return NextResponse.json({ message: "All Blogs", data: blogs }, { status: 200 });
+
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+}
