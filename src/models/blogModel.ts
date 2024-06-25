a3c561ac922929d48ac42527c52dc9719d55e1a9
kashish567 <kashishbhanushali7@gmail.com>
2024-06-25 20:33:50 +0530

    feat: api changes for storing user with id and reference of blog with user id

--- src/models/blogModel.ts
+++ src/models/blogModel.ts
@@ -1,22 +1,25 @@
 import mongoose from "mongoose";
- 
-const blogSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    caption: {
-        type: String,
-        required: true,
-    },
-    image :{
-        type : String,
-    },
-    // user:{
-    //     type: mongoose.Schema.Types.ObjectId,
-    //     ref: "users",
-    // }
-},{timestamps:true})
 
-const Blog = mongoose.models.blogs || mongoose.model("blogs", blogSchema)
+const blogSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    caption:{
+        type: String,
+        required: true,
+    },
+    img:{
+        type: String,
+        required: true
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    }
+},{timestamps: true});
+
+
+const Blog = mongoose.models.blogs || mongoose.model("blogs",blogSchema);
+
 export default Blog;
