2f34746e6f11fd3d6fe8a119c865db44d8a51729
kashish567 <kashishbhanushali7@gmail.com>
2024-06-25 19:00:07 +0530

    feat: api for the login route made

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -2,8 +2,37 @@ import dbConnection from "@/config/dbConnect";
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModel";
 import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken";
 
 dbConnection()
-export const POST=(req : NextRequest)=>{
-    
+export const POST= async(req : NextRequest)=>{
+    try {
+        const body = await req.json();
+        const {email, password} = body;
+        if(!email && !password)
+            return NextResponse.json({message : "Please enter all details"}, {status:400})
+        
+        const user = await User.findOne({email});
+        if(!user)
+            return NextResponse.json({message : "User does not exist"}, {status:400})
+
+        const isMatch = await bcryptjs.compare(password, user.password)
+        if(!isMatch)
+            return NextResponse.json({message : "Invalid credentials"}, {status:400})
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET!, { expiresIn: "1d" });
+
+        const res = NextResponse.json(
+            { message: `Welcome back ${user.username}`, success: true },
+            { status: 200 }
+        );
+
+        // Set the cookie
+        res.cookies.set("token", token, { httpOnly: true });
+
+        return res;
+
+    } catch (error:any) {
+        return NextResponse.json({message:error.message}, {status:500})
+    }
 }
